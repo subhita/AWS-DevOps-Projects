e5a48c40921fdfe8a294c3094df0b1a9ff2ca7bc
Subhita <37235520+subhita@users.noreply.github.com>
2024-07-23 13:19:37 -0300

    first python app

--- Python-App-AWS-CI-Process/app.py
+++ Python-App-AWS-CI-Process/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!'
+    return 'Hello, This is my first python app using CI process and AWS'
 
 if __name__ == '__main__':
     app.run()
